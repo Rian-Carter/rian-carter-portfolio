2de091e33cb7daab3b613ab57f55c06419138304
Rian Carter <R.Carter86@hotmail.com>
2022-07-15 15:56:34 -0700

    WIP

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
-const hamburgerButton = document.getElementById('hamburger')
-const navList = document.getElementById('nav-list')
+// const hamburgerButton = document.getElementById('hamburger')
+// const navList = document.getElementById('nav-list')
 
-function toggleButton() {
-    navList.classList.toggle('show')
-}
+// function toggleButton() {
+//     navList.classList.toggle('show')
+// }
 
-hamburgerButton.addEventListener('click', toggleButton)
+// hamburgerButton.addEventListener('click', toggleButton)
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
